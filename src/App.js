62ec9123312e03a8de6bc4892ead70d625062b1e
Charles Van den Bergh <vandenberghcharles@gmail.com>
2020-06-15 15:02:51 +0200

    first try

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <button>-</button>
+      <span>0</span>
+      <button>+</button>
+    </>
+  );
+}
+
+export default App;
