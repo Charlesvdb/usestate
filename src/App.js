70b35bb5422987f444b0c65282dfd9f666cb5f7c
Charles Van den Bergh <vandenberghcharles@gmail.com>
2020-06-16 00:43:47 +0200

    first try

--- src/App.js
+++ src/App.js
@@ -1,37 +1,56 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 
-
-//USEEFFECT 
-
 export default function App(){
-  const [resourceType, setResourceType] = useState("posts")
-  const [items, setItems] = useState([])
+  const [windowWidth, setwindowWidth] = useState(window.innerWidth)
+  
+  const handleResize = () => {
+    setwindowWidth(window.innerWidth)
+  }
 
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-    .then(response => response.json())
-    .then(json => setItems(json))
-  }, [resourceType])
+      window.addEventListener("resize",handleResize)
+
+      return () => {
+        console.log("return from resource change")
+      }
+  }, [])
 
   return (
-    <>
-      <div>
-        <button onClick={() => setResourceType("posts")}>Posts</button>
-        <button onClick={() => setResourceType("users")}>Users</button>
-        <button onClick={() => setResourceType("comments")}>Comments</button>
-      </div>
-      <h1>{resourceType}</h1>
-      {items.map(item => {
-        return <pre>{JSON.stringify(item)}</pre>
-      })}
-    </>
-
+      <div>{windowWidth}</div>
   )
 }
 
 
 
+
+//USEEFFECT 
+
+// export default function App(){
+//   const [resourceType, setResourceType] = useState("posts")
+//   const [items, setItems] = useState([])
+
+//   useEffect(() => {
+//     fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+//     .then(response => response.json())
+//     .then(json => setItems(json))
+//   }, [resourceType])
+
+//   return (
+//     <>
+//       <div>
+//         <button onClick={() => setResourceType("posts")}>Posts</button>
+//         <button onClick={() => setResourceType("users")}>Users</button>
+//         <button onClick={() => setResourceType("comments")}>Comments</button>
+//       </div>
+//       <h1>{resourceType}</h1>
+//       {items.map(item => {
+//         return <pre></pre>
+//       })}
+//     </>
+
+//   )
+// }
 
 
 //USESTATE 
