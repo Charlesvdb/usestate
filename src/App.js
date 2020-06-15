6681bb1d1420d59cab95e8b309d139c735b9eb15
Charles Van den Bergh <vandenberghcharles@gmail.com>
2020-06-15 16:12:46 +0200

    first try

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [count,setCount] = useState(4)
+  const [theme,setTheme] = useState("blue")
+
+
+  function decrementCount(){
+    setCount(prevCount => prevCount - 1)
+  }
+
+  function incrementCount(){
+    setCount(prevCount => prevCount + 1)
+    setTheme("red")
+  }
+
   return (
     <>
-      <button>-</button>
-      <span>0</span>
-      <button>+</button>
+      <button onClick={decrementCount}>-</button>
+      <span>{count}</span>
+      <span>{theme}</span>
+      <button onClick={incrementCount}>+</button>
     </>
   );
 }
